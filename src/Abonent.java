c7ab2e419155f17f39e06a53bc987f384554a732
Elvida2004 <147031392+Elvida2004@users.noreply.github.com>
2024-10-31 16:42:40 +0300

    забыл

--- src/Abonent.java
+++ src/Abonent.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Abonent {
+public class Abonent {
+    private int id;
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String address;
+    private int all_phone_times;
+    private int creditcard_number;
+    private int debt;
+
+    public Abonent(int id, String name, String surname, String patronymic, String address, int all_phone_times, int creditcard_number, int debt) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.all_phone_times = all_phone_times;
+        this.creditcard_number = creditcard_number;
+        this.debt = debt;
+    }
+
 }
